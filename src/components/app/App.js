cfcc710134de4a4b1897ac0879481ce443af760f
Nicolas Jalil <gjherrera.i@gmail.com>
2019-08-01 13:51:30 -0600

    Fixed issue with handler functions. Added an action button to cypher.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import cypherOptionList from '../assets/cypherOptionList.json'
 import axios from 'axios';
-import _debounce from 'lodash/debounce'
 
 function App() {
   const [cypher, setCypher] = useState(null)
@@ -10,22 +9,33 @@ function App() {
   const [cypheredText, setCypheredText] = useState('You\'ve yet to input something...')
 
   const[isTextAreaDisabled, setIsTextAreaDisabled] = useState(true)
+  
+  const[isRequestButtonDisabled, setIsRequestButtonDisabled] = useState('disabled')
 
   const[text, setText] = useState('default')
   
-  const handleSelectChange = event => {
+  function handleSelectChange (event) {
+    console.log(event.target.value)
     setIsTextAreaDisabled(false)
     setCypher(event.target.value)
   }
 
-  const handleTextAreaChange = _debounce((value) => {    
-    setText(value)
-    getCypheredText();
-  },500)
+  function handleTextAreaChange (event) {
+    if(event.target.value === '') {
+      setCypheredText('Dont\'t you want to cypher something? :/')
+      setIsRequestButtonDisabled('disabled')
+    } else {
+      setText(event.target.value)
+      setIsRequestButtonDisabled('')
+    }
+  }
 
+  function handleCypherAction (event) {
+    getCypheredText()
+  }
 
   const getCypheredText = () => {
-    axios.get('/'+cypher+'?text=')
+    axios.get('/'+cypher+'?text='+text)
     .then(function (response) {
       setCypheredText(response.data)
     }).catch(function(error) {
@@ -36,8 +46,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cypher any text!</h1>
-        <h3>Choose your cypher</h3>        
+        <h1 className="cypher-h1">Cypher any text!</h1>
+        <h3 className="cypher-h3">Choose your cypher</h3>        
         <select className="cypher-select" onChange={handleSelectChange}>
           <option disabled selected>Select a cypher</option>
           {
@@ -46,7 +56,8 @@ function App() {
           }
         </select>
         <textarea disabled={isTextAreaDisabled} className="cypher-input-text" onChange={handleTextAreaChange} placeholder="Input the text you want to cypher."></textarea>
-        <h4>Cyphered Result</h4>
+        <div className="button_cont" align="center"><a className={'cypher-button'+' '+isRequestButtonDisabled} onClick={handleCypherAction}>Cypher Text!</a></div>
+        <h3>Cyphered Result:</h3>
         <label className="cypher-result">{cypheredText}</label>
       </header>
       <footer className="App-footer"></footer>
