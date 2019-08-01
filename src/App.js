2c559932b6d9eca44ea8e8b0e5211ed7206c33aa
Nakdimon <gjherrera.i@gmail.com>
2019-07-31 18:41:58 -0600

    Installing depencies and creating basic UI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cypherOptionList from './cypherOptionList'
+// import axios from 'axios';
+// import _debounce from 'lodash/debounce'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cypher any text!</h1>
+        <h3>Choose your cypher</h3>        
+        <select className="cypher-select">
+          <option disabled selected>Select a cypher</option>
+          {cypherOptionList.map((cypherOption) => 
+            <option className="cypher-option" key={cypherOption.value} value={cypherOption.value}>{cypherOption.label}</option>
+           )
+          }
+        </select>
+        <textarea className="cypher-input-text" placeholder="Input the text you want to cypher."></textarea>
+        <h4>Cyphered Result</h4>
+        <label className="cypher-result">result</label>
       </header>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
