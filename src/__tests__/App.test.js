7e3a1b6559523d483fb91907b4814efc20ebb880
Nicolas Jalil <gjherrera.i@gmail.com>
2019-08-02 14:50:31 -0600

    Added basic App component tests.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../components/app/App';
+import { shallow }  from '../enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("App component: ", ()=> { 
+  it("Renders.", () => {
+    const component = shallow(<App />)
+    expect(component.exists()).toBe(true)
+  }),
+
+  it("Matches the snapshot.", () => {
+    const component = shallow(<App />)
+    expect(component).toMatchSnapshot();
+  })
+})
