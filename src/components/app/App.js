ea3dd1845a2d422219e4edc0c45fc7d52a812db2
Nakdimon <gjherrera.i@gmail.com>
2019-08-01 18:25:26 -0600

    Formatted App.css. Replaced static elements with Components.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import cypherOptionList from '../assets/cypherOptionList.json'
+import Select from '../select/Select'
+import TextArea from '../textArea/TextArea'
+import Button from '../button/Button'
 import axios from 'axios';
 
 function App() {
@@ -15,7 +18,6 @@ function App() {
   const[text, setText] = useState('default')
   
   function handleSelectChange (event) {
-    console.log(event.target.value)
     setIsTextAreaDisabled(false)
     setCypher(event.target.value)
   }
@@ -47,16 +49,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="cypher-h1">Cypher any text!</h1>
-        <h3 className="cypher-h3">Choose your cypher</h3>        
-        <select className="cypher-select" onChange={handleSelectChange}>
-          <option disabled selected>Select a cypher</option>
-          {
-            cypherOptionList.map((cypherOption) => 
-            <option className="cypher-option" key={cypherOption.value} value={cypherOption.value}>{cypherOption.label}</option>)
-          }
-        </select>
-        <textarea disabled={isTextAreaDisabled} className="cypher-input-text" onChange={handleTextAreaChange} placeholder="Input the text you want to cypher."></textarea>
-        <div className="button_cont" align="center"><a className={'cypher-button'+' '+isRequestButtonDisabled} onClick={handleCypherAction}>Cypher Text!</a></div>
+        <h3 className="cypher-h3">Choose your cypher</h3>
+        <Select 
+          cypherOptionList={cypherOptionList} 
+          handleSelectChange={handleSelectChange}
+        />        
+        <TextArea
+          isTextAreaDisabled={isTextAreaDisabled}
+          handleTextAreaChange={handleTextAreaChange}
+        />        
+        <Button
+          isRequestButtonDisabled={isRequestButtonDisabled}
+          handleCypherAction={handleCypherAction}
+        />        
         <h3>Cyphered Result:</h3>
         <label className="cypher-result">{cypheredText}</label>
       </header>
